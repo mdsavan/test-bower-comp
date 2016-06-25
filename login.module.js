ab45a5814c3039d3c610b00cbd140a923a8d8bc6
Savan Koradia <savan.koradia@multidots.in>
2016-06-25 15:17:23 +0530

    sample login

---
+++ login.module.js
@@ -0,0 +1,11 @@
+(function(angular){
+    'use strict';
+
+    angular.module('testLogin', [])
+        .component('testLogin', {
+            'templateUrl' : '../bower_components/test-bower-comp/test-login.template.html',
+            'controller': function(){
+            }
+        });
+
+})(window.angular);
